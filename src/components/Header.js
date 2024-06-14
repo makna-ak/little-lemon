78543e404b3b4d3e4f2545aa40a6258dd2462a9f
makna-ak <maknaak13@gmail.com>
2024-06-14 22:47:12 +0700

    add header and new meta ogp

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+    return(
+        <header>
+            <img src="/Logo.svg" alt="Little lemon logo" />
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
