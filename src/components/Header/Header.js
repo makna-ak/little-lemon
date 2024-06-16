1fbd71263dbc387b844c00effa90596af2d4bb23
makna-ak <maknaak13@gmail.com>
2024-06-16 13:51:37 +0700

    automate card list and fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return(
         <header>
-            <img src="/Logo.svg" alt="Little lemon logo" />
+            <Link to={`/`}><img src="/Logo.svg" alt="Little lemon logo" /></Link>
             <nav>
                 <ul className='karla-text'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Menu</li>
-                    <li>Reservations</li>
-                    <li>Order Online</li>
-                    <li>Login</li>
+                    <li><Link>Home</Link></li>
+                    <li><Link>About</Link></li>
+                    <li><Link>Menu</Link></li>
+                    <li><Link>Reservations</Link></li>
+                    <li><Link>Order Online</Link></li>
+                    <li><Link>Login</Link></li>
                 </ul>
             </nav>
         </header>
--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -1,6 +1,43 @@
 import './Highlights.css'
 
 function Highlights() {
+
+    const cards = [
+        {
+            img: '/assets/greek salad.jpg',
+            name: 'Greek Salad',
+            price: '$12.99',
+            description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'
+        },
+        {
+            img: '/assets/bruchetta.svg',
+            name: 'Bruchetta',
+            price: '$5.99',
+            description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
+        },
+        {
+            img: '/assets/lemon dessert.jpg',
+            name: 'Lemon Dessert',
+            price: '$5.00',
+            description: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.'
+        },
+    ]
+
+    const listCards = cards.map((item) => {
+        return(
+        <article className='card'>
+            <img src={item.img} alt={item.name} />
+            <div className='card-description karla-text'>
+                <div className='card-header'>
+                    <h4 className='title markazi-text'>{item.name}</h4>
+                    <p className='price'>{item.price}</p>
+                </div>
+                <p className='description'>{item.description}</p>
+                <button className='order-btn karla-text'>Order a delivery</button>
+            </div>
+        </article>
+        )
+    })
     return(
         <section className='Highlights'>
             <div className='highlight-header'>
@@ -8,48 +45,7 @@ function Highlights() {
                 <button className='primary-btn karla-text'>Online Menu</button>
             </div>
             <div className='card-container'>
-                <article className='card'>
-                    <img src="/assets/greek salad.jpg" alt="Greek Salad" />
-                    <div className='karla-text'>
-                        <div className='card-header'>
-                            <h4 className='title markazi-text'>Greek Salad</h4>
-                            <p className='price'>$12.99</p>
-                        </div>
-                        <p className='card-description'>The famous greek salad of crispy lettuce, peppers, 
-                            olives and our Chicago style feta cheese, 
-                            garnished with crunchy garlic and rosemary croutons.
-                        </p>
-                        <button className='order-btn karla-text'>Order a delivery</button>
-                    </div>
-                </article>
-                <article className='card'>
-                    <img src="/assets/greek salad.jpg" alt="Greek Salad" />
-                    <div className='karla-text'>
-                        <div className='card-header'>
-                            <h4 className='title markazi-text'>Greek Salad</h4>
-                            <p className='price'>$12.99</p>
-                        </div>
-                        <p className='card-description'>The famous greek salad of crispy lettuce, peppers, 
-                            olives and our Chicago style feta cheese, 
-                            garnished with crunchy garlic and rosemary croutons.
-                        </p>
-                        <button className='order-btn karla-text'>Order a delivery</button>
-                    </div>
-                </article>
-                <article className='card'>
-                    <img src="/assets/greek salad.jpg" alt="Greek Salad" />
-                    <div className='karla-text'>
-                        <div className='card-header'>
-                            <h4 className='title markazi-text'>Greek Salad</h4>
-                            <p className='price'>$12.99</p>
-                        </div>
-                        <p className='card-description'>The famous greek salad of crispy lettuce, peppers, 
-                            olives and our Chicago style feta cheese, 
-                            garnished with crunchy garlic and rosemary croutons.
-                        </p>
-                        <button className='order-btn karla-text'>Order a delivery</button>
-                    </div>
-                </article>
+                {listCards}
             </div>
         </section>
     )
