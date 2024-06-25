380d9f11e890dd47314d422879e4d0a158d15a56
makna-ak <maknaak13@gmail.com>
2024-06-26 05:37:57 +0700

    add booking page

--- src/routes/Booking.jsx
+++ src/routes/Booking.jsx
@@ -1,7 +1,34 @@
+import '../index.css'
+
 function Booking() {
     return(
         <>
-            <h1>Hello world!</h1>
+            <section className="Booking">
+                <div>
+                    <h1 className='markazi-text'>Reservation</h1>
+                    <form action="" className='karla-text'>
+                        <label htmlFor="date">Choose date</label>
+                        <input type="date" name="date" id="date" />
+                        <label htmlFor="time">Choose time</label>
+                        <select name="time" id="time">
+                            <option value="">17:00</option>
+                            <option value="">18:00</option>
+                            <option value="">19:00</option>
+                            <option value="">20:00</option>
+                            <option value="">21:00</option>
+                            <option value="">22:00</option>
+                        </select>
+                        <label htmlFor="guests">Number of guests</label>
+                        <input type="range" min={0} max={10} name="guests" id="guests" />
+                        <label htmlFor="occasion">Occasion</label>
+                        <select name="occasion" id="occasion">
+                            <option value="">Birthday</option>
+                            <option value="">Anniversary</option>
+                        </select>
+                        <button className='primary-btn' type="submit">Make your reservation</button>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
